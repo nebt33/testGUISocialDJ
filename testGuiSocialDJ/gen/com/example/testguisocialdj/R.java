338a6e9fd86bad452b799ebbf92044aea92834ee
nebt33 <nebt33@mst.edu>
2014-03-06 00:15:28 -0600

    Redid Connect screen -now has radio buttons -only can connect to on server at a time -added textviews to appear when radio is clicked on
    --need to make errors still

--- testGuiSocialDJ/gen/com/example/testguisocialdj/R.java
+++ testGuiSocialDJ/gen/com/example/testguisocialdj/R.java
@@ -32,22 +32,25 @@ public final class R {
         public static final int queue=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
+        public static final int action_settings=0x7f09000d;
         public static final int browseButton=0x7f090006;
         public static final int browseButtontextView=0x7f090007;
+        public static final int code=0x7f09000b;
         public static final int connectButton=0x7f090004;
         public static final int connectButtontextView=0x7f090005;
         public static final int controlsButton=0x7f090001;
         public static final int controlsButtontextView=0x7f090000;
         public static final int infoButton=0x7f090008;
         public static final int infotextView=0x7f090009;
-        public static final int listView=0x7f09000a;
+        public static final int listView1=0x7f09000c;
         public static final int playlistButton=0x7f090003;
         public static final int playlistButtontextView=0x7f090002;
+        public static final int radio=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_queue=0x7f030001;
+        public static final int connect_main=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
@@ -59,6 +62,7 @@ public final class R {
         public static final int Controls=0x7f050002;
         public static final int Info=0x7f050006;
         public static final int Playlist=0x7f050003;
+        public static final int Servers=0x7f050008;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int title_activity_queue=0x7f050007;
--- testGuiSocialDJ/src/com/example/testguisocialdj/QueueActivity.java
+++ testGuiSocialDJ/src/com/example/testguisocialdj/QueueActivity.java
@@ -21,16 +21,23 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,67 +49,31 @@ public class QueueActivity extends Activity {
 	//standard port for not active ip address on network
 	private static String nonActiveIp = "0.0.0.0";
 	//Adapter
-	private static SimpleAdapter simpleAdpt;
+	private static MyAdapter myAdpt;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_queue);
+		setContentView(R.layout.connect_main);
 		
-		List<Map<String,String>> activeServers = findHosts();
+		ArrayList<IP> activeServers = findHosts();
 			   
-		ListView list = (ListView)findViewById(R.id.listView);
+		ListView listView = (ListView)findViewById(R.id.listView1);
 		
-		simpleAdpt = new SimpleAdapter(this, activeServers, android.R.layout.simple_list_item_1, new String[] {"host"}, new int[] {android.R.id.text1});
+		myAdpt = new MyAdapter(this, R.layout.activity_queue, activeServers);
 		
-		list.setAdapter(simpleAdpt);
-
-		//register contextmenu
-		registerForContextMenu(list);	
-	}
-	
-	/**
-	 * Context Menu when user long clicks on a listview item
-	 */
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-
-		super.onCreateContextMenu(menu, v, menuInfo);
-		AdapterContextMenuInfo aInfo = (AdapterContextMenuInfo) menuInfo;
-
-		// We know that each row in the adapter is a Map
-		HashMap map =  (HashMap) simpleAdpt.getItem(aInfo.position);
-
-
-		menu.setHeaderTitle("Options for Server " + map.get("host"));
-		menu.add(1, 1, 1, "Connect");
-		menu.add(1, 2, 2, "Disconnect");
-	}
-	
-	/**
-	 * Method that will control what happens when an item is clicked in the 
-	 * contextMenu
-	 */
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		int itemId = item.getItemId();
+		listView.setAdapter(myAdpt);
 		
-		//connect
-		if(itemId == 1) {
-			AdapterContextMenuInfo aInfo = (AdapterContextMenuInfo) item.getMenuInfo();
-			HashMap map =  (HashMap) simpleAdpt.getItem(aInfo.position);
-			try {
-				Socket socket = new Socket();
-				socket.connect(new InetSocketAddress(map.get("host").toString(), standardPort), 200);
-
-			} catch (Exception ex) {return false;}
-		}
-		//disconnect
-		else {
-			
-		}
-		return true;
+		/*listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				IP ip = (IP) parent.getItemAtPosition(position);
+				Toast.makeText(getApplicationContext(),
+						"Clicked on Row: " + ip.getAddress(), 
+						Toast.LENGTH_LONG).show();
+			}
+		});*/
 	}
 
 
@@ -113,7 +84,7 @@ public class QueueActivity extends Activity {
 		return true;
 	}
 	
-	public List<Map<String,String>> findHosts(){
+	public ArrayList<IP> findHosts(){
 		/*
 		 * Gets the address of router connected to.
 		 */
@@ -122,7 +93,7 @@ public class QueueActivity extends Activity {
 		final String address = Formatter.formatIpAddress(dhcp.gateway);
 
 		//list of active servers on network
-		List<Map<String,String>> activeServers = new ArrayList<Map<String,String>>();
+		ArrayList<IP> activeServers = new ArrayList<IP>();
 		
 		try {
 			NetworkInterface iFace = NetworkInterface
@@ -144,7 +115,8 @@ public class QueueActivity extends Activity {
 		  for (final Future<String> f : futures) {
 		    try {
 				if (!f.get().equals(nonActiveIp)) {
-					activeServers.add(createHost("host",f.get()));
+					IP ip = new IP(f.get(), false);
+					activeServers.add(ip);
 				}
 			} catch (InterruptedException e) {e.printStackTrace();
 			} catch (ExecutionException e) {}
@@ -176,10 +148,90 @@ public class QueueActivity extends Activity {
 			}
 		});
 	}
-	
-	private HashMap<String, String> createHost(String key, String id) {
-		HashMap<String, String> host = new HashMap<String,String>();
-		host.put(key, id);
-		return host;
+
+	private class MyAdapter extends ArrayAdapter<IP> {
+		ArrayList<IP> activeServers;
+		private RadioButton currentlyClicked;
+		private boolean check = false;
+
+		public MyAdapter(Context context, int textViewResourceId, 
+				ArrayList<IP> activeServers) {
+			super(context, textViewResourceId, activeServers);
+			this.activeServers = new ArrayList<IP>();
+			this.activeServers.addAll(activeServers);
+		}
+
+		private class ViewHolder {
+			TextView code;
+			RadioButton name;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			 ViewHolder holder = null;
+			   Log.v("ConvertView", String.valueOf(position));
+			 
+			   if (convertView == null) {
+			   LayoutInflater vi = (LayoutInflater)getSystemService(
+			     Context.LAYOUT_INFLATER_SERVICE);
+			   convertView = vi.inflate(R.layout.activity_queue, null);
+			 
+			   holder = new ViewHolder();
+			   holder.code = (TextView) convertView.findViewById(R.id.code);
+			   holder.name = (RadioButton) convertView.findViewById(R.id.radio);
+			   convertView.setTag(holder);
+			   
+			   //temp variable to set to final to use when radio button is clicked on
+			   final TextView temp = holder.code;
+			   
+			   if (position == getCount() - 1 && check == false) {
+		            holder.name.setChecked(true);
+		            currentlyClicked = holder.name;
+		        } else {
+		            holder.name.setChecked(false);
+		        }
+			 
+			    holder.name.setOnClickListener( new View.OnClickListener() {  
+			     public void onClick(View v) {  
+			    	 if (currentlyClicked != null) {
+		                    if (currentlyClicked == null)
+		                    	currentlyClicked = (RadioButton) v;
+		                    currentlyClicked.setChecked(true);
+		                }
+
+		                if (currentlyClicked == v)
+		                    return;
+		                
+		                System.out.println(currentlyClicked.getId());
+		                System.out.println(temp.getText());
+		                
+		                currentlyClicked.setChecked(false);
+		                ((RadioButton) v).setChecked(true);
+		                currentlyClicked = (RadioButton) v;
+
+		                try {
+		                  Socket socket = new Socket();
+						  socket.connect(new InetSocketAddress(temp.getText().toString().trim(), standardPort), 200);
+						  Toast.makeText(getApplicationContext(), "Connected to Server: " + temp.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+		                } catch (Exception ex) {
+                             //server can't be connected to
+		                	Toast.makeText(getApplicationContext(), "Error Connecting to Server: " + temp.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+						}
+			     }  
+			    });  
+			   } 
+			   else {
+			    holder = (ViewHolder) convertView.getTag();
+			   }
+			 
+			   IP ip = activeServers.get(position);
+			   holder.code.setText("   " +  ip.getAddress());
+			   holder.name.setChecked(ip.isSelected());
+			 
+			   return convertView;
+			 
+		}
+
 	}
 }
